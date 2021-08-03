d2b8775a06852529d1ab1233fa79c871969dc067
XiaNengQi <920986617@qq.com>
2021-08-03 20:11:03 +0800

    first Color extensions

---
+++ SwiftUIFulCrypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  SwiftUIFulCrypto
+//
+//  Created by 夏能啟 on 8/3/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
