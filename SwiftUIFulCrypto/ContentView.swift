4f485f839c170abceaa8926aa513f97a81908b86
XiaNengQi <920986617@qq.com>
2021-08-03 19:33:23 +0800

    Initial Commit

---
+++ SwiftUIFulCrypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFulCrypto
+//
+//  Created by 夏能啟 on 8/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFulCrypto/SwiftUIFulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFulCryptoApp.swift
+//  SwiftUIFulCrypto
+//
+//  Created by 夏能啟 on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
