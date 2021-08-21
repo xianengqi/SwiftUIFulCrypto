9e8889db205e342122380523dda9d13f48edde8c
XiaNengQi <920986617@qq.com>
2021-08-21 20:37:21 +0800

    Filtering data base on search

--- SwiftUIFulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUIFulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,12 +22,51 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-        // `weak self`是弱引用
+//        dataService.$allCoins
+//        // `weak self`是弱引用
+//            .sink { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//            }
+//            .store(in: &cancelables)
+//
+        
+        /// updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+        // 添加去抖动，··假如用户快速输入10次的时候可能要在数据库请求10次，如果数据库量大那就会在屏幕上体验不好卡顿。
+        // 所以给添加防抖
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+        ///   把这串代码封装成一个私有函数`filterCoins`，方便调用
+//            .map { (text, startingCoins) -> [CoinModel] in
+//                guard !text.isEmpty else {
+//                    return startingCoins
+//                }
+//
+//                let lowercasedText = text.lowercased() // 转换为小写
+//                return startingCoins.filter { (coin) -> Bool in
+//                    return coin.name.lowercased().contains(lowercasedText) ||
+//                    coin.symbol.lowercased().contains(lowercasedText) ||
+//                    coin.id.lowercased().contains(lowercasedText)
+//                }
+//            }
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancelables)
+    }
+    
+    private func filterCoins(text: String, coin: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coin
+        }
+        
+        let lowercasedText = text.lowercased() // 转换为小写
+        return coin.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
     
 }
